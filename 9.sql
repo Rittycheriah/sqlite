52244179ce94d98ab43c3783ca4a2cc7d8e6b19f
rittycheriah <rittycheriah@gmail.com>
2015-09-15 15:36:39 -0500

    Solution 9

--- 9.sql
+++ 9.sql
@@ -1,1 +1,2 @@
-SELECT COUNT(InvoiceId) FROM Invoice WHERE InvoiceDate='2009[]' OR '2010[]';
+SELECT SUM(I.Total), Count(I.InvoiceId) FROM Invoice I WHERE I.InvoiceDate BETWEEN '2011-01-01' AND '2011-12-31' 
+UNION SELECT SUM(I.Total), Count(I.InvoiceId) FROM Invoice I WHERE I.InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31';
